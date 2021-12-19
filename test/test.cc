d2998e23026fa0e69716ae473bd56e3a83db9b0e
Ryohji Ikebe <Ryohji.Ikebe@gmail.com>
2021-12-20 02:01:46 +0900

    Add first silly implementation.

--- test/test.cc
+++ test/test.cc
@@ -1,23 +1,15 @@
 #include <gtest/gtest.h>
 
+#include <string>
+
 namespace {
-int factorial(int x) {
-    auto v = 1;
-    for (; x; x -= 1)
-        v *= x;
-    return v;
+
+double eval(const std::string &expr) {
+    return 1;
 }
+
 } // namespace
 
-// Tests factorial of 0.
-TEST(FactorialTest, HandlesZeroInput) {
-    EXPECT_EQ(1, factorial(0));
-}
-
-// Tests factorial of positive numbers.
-TEST(FactorialTest, HandlesPositiveInput) {
-    EXPECT_EQ(1, factorial(1));
-    EXPECT_EQ(2, factorial(2));
-    EXPECT_EQ(6, factorial(3));
-    EXPECT_EQ(40320, factorial(8));
+TEST(Expression, one_is_evaluated_to_1) {
+    ASSERT_EQ(1, eval("1"));
 }
