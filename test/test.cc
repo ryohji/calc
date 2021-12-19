0b8b65102ab80dae0372785f4e440b64711a5736
Ryohji Ikebe <Ryohji.Ikebe@gmail.com>
2021-12-20 02:02:52 +0900

    Add 2nd test case; test fails.

--- test/test.cc
+++ test/test.cc
@@ -10,6 +10,10 @@ double eval(const std::string &expr) {
 
 } // namespace
 
+TEST(Expression, zero_is_evaluated_to_0) {
+    ASSERT_EQ(0, eval("0"));
+}
+
 TEST(Expression, one_is_evaluated_to_1) {
     ASSERT_EQ(1, eval("1"));
 }
